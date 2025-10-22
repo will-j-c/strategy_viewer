44cf14bfd09f4d3df4a03bf743d93276eb4115d1
Will <w.j.colgate@gmail.com>
2025-10-22 17:55:01 +0700

    skeleton for mono repo and initial hookup

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <Link href="/api/python">
+            <code className="font-mono font-bold">api/index.py</code>
+          </Link>
+        </p>
+      </main>
+    </div>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
+};
+
+export default nextConfig;
